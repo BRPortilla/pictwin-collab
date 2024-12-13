b95ed7df93d84fbf4b8006f24c5d9da073cac984
BaironR <blrojas2668@gmail.com>
2024-12-13 20:23:03 -0300

    feat(TheMain): implement TheMain class for the warehouse

--- warehouse/src/main/java/cl/ucn/disc/dsm/pictwin/TheMain.java
+++ warehouse/src/main/java/cl/ucn/disc/dsm/pictwin/TheMain.java
@@ -1,4 +1,52 @@
 package cl.ucn.disc.dsm.pictwin;
 
+import cl.ucn.disc.dsm.pictwin.model.Persona;
+import cl.ucn.disc.dsm.pictwin.model.PicTwin;
+import cl.ucn.disc.dsm.pictwin.services.Controller;
+import cl.ucn.disc.dsm.pictwin.utils.FileUtils;
+
+import io.ebean.DB;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.File;
+import java.util.List;
+
+/** The Main. */
+@Slf4j
 public class TheMain {
+
+    /** Starting point. */
+    public static void main(String[] args) {
+
+        log.debug("Starting TheMain ..");
+
+        // the controller
+        Controller c = new Controller(DB.getDefault());
+
+        // seed the database
+        if (c.seed()){
+            log.debug("Seeded the database.");
+        }
+
+        log.debug("Registering Persona ..");
+        Persona p = c.register("bairon.rojas@alumnos.ucn.cl","brojas123");
+        log.debug("Persona: {}", p);
+
+        File file = FileUtils.getResourceFile("antofagasta.jpg");
+        log.debug("File: {}", file);
+
+        PicTwin pt = c.addPic(p.getUlid(), -23.6509, -70.9975, file);
+        log.debug("PicTwin: {}", pt);
+
+        Persona p2 = c.login("bairon.rojas@alumnos.ucn.cl", "brojas123");
+        log.debug("Persona: {}", p2);
+
+        List<PicTwin> pts = c.getPicTwins(p.getUlid());
+        for (PicTwin ptt: pts){
+            log.debug("PicTwin: {}", ptt);
+        }
+
+        log.debug("Done.");
+    }
 }
