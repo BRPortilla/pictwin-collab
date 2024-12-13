622ef03aa7b563e2c43f8432f159e8a11762937f
BaironR <blrojas2668@gmail.com>
2024-12-13 19:05:07 -0300

    feat(PicTwin): implement class PicTwin, to link 2 Pics

--- warehouse/src/main/java/cl/ucn/disc/dsm/pictwin/model/PicTwin.java
+++ warehouse/src/main/java/cl/ucn/disc/dsm/pictwin/model/PicTwin.java
@@ -1,4 +1,45 @@
 package cl.ucn.disc.dsm.pictwin.model;
 
+import io.ebean.annotation.NotNull;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.time.Instant;
+
+/** The PicTwin. */
+@Getter
+@ToString(callSuper = true)
+@AllArgsConstructor
+@Builder
+@Entity
 public class PicTwin {
+
+    /** The Expiration. */
+    @NotNull private Instant expiration;
+
+    /** The Expired. */
+    @Builder.Default @NotNull private Boolean expired = Boolean.FALSE;
+
+    /** The Reported. */
+    @Builder.Default @NotNull private Boolean reported = Boolean.FALSE;
+
+    /** The Persona relationship. */
+    @ManyToOne(optional = false)
+    private Persona persona;
+
+    /** The Pic relationship. */
+    @ToString.Exclude
+    @ManyToOne(optional = false)
+    private Pic pic;
+
+    /** The Pic relationship. */
+    @ToString.Exclude
+    @ManyToOne(optional = false)
+    private Pic twin;
 }
