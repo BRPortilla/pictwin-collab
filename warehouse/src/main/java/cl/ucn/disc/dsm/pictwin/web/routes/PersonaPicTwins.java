7a85a9faf4f16e409f8e13f67631f8bc7d9a9e26
BaironR <blrojas2668@gmail.com>
2024-12-13 20:15:20 -0300

    feat(PersonaPicTwins): implement PersonaPicTwins route for PicTwins of the Persona

--- warehouse/src/main/java/cl/ucn/disc/dsm/pictwin/web/routes/PersonaPicTwins.java
+++ warehouse/src/main/java/cl/ucn/disc/dsm/pictwin/web/routes/PersonaPicTwins.java
@@ -1,4 +1,31 @@
 package cl.ucn.disc.dsm.pictwin.web.routes;
 
-public class PersonaPicTwins {
+import cl.ucn.disc.dsm.pictwin.services.Controller;
+import cl.ucn.disc.dsm.pictwin.web.Route;
+
+import io.javalin.http.Handler;
+
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+
+/** The PicTwins of Persona route. */
+@Slf4j
+public final class PersonaPicTwins extends Route {
+
+    /** The Constructor. */
+    public PersonaPicTwins(@NonNull final Controller controller) {
+        super(Route.Method.GET, "api/personas/{ulid}/pictwins");
+
+        this.handler = buildHandler(controller);
+    }
+
+    /** Build the handler. */
+    private static Handler buildHandler(Controller controller){
+        return ctx -> {
+            String ulid = ctx.pathParam("ulid");
+            log.debug("Detected ulid={} for Persona.", ulid);
+
+            ctx.json(controller.getPicTwins(ulid));
+        };
+    }
 }
