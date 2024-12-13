313af8dc27ab292a11189892be6dca35dbf5b8e4
BaironR <blrojas2668@gmail.com>
2024-12-13 20:19:35 -0300

    feat(TheServer): implement the Javalin web server to initiate the API

--- warehouse/src/main/java/cl/ucn/disc/dsm/pictwin/TheServer.java
+++ warehouse/src/main/java/cl/ucn/disc/dsm/pictwin/TheServer.java
@@ -1,4 +1,65 @@
 package cl.ucn.disc.dsm.pictwin;
 
+import cl.ucn.disc.dsm.pictwin.services.Controller;
+import cl.ucn.disc.dsm.pictwin.web.Route;
+import cl.ucn.disc.dsm.pictwin.web.routes.Home;
+import cl.ucn.disc.dsm.pictwin.web.routes.PersonaLogin;
+import cl.ucn.disc.dsm.pictwin.web.routes.PersonaPic;
+import cl.ucn.disc.dsm.pictwin.web.routes.PersonaPicTwins;
+
+import io.ebean.DB;
+import io.javalin.Javalin;
+
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CountDownLatch;
+
+/** The Server. */
+@Slf4j
 public class TheServer {
+
+    /** The Javalin web server. */
+    private static Javalin createJavalin() {
+
+        // create and configure the Javalin web server
+        return Javalin.create(
+                config -> {
+                    // enable the extensive logging
+                    config.requestLogger.http(
+                            (ctx, ms) -> {
+                                log.debug("Served {} in {} ms", ctx.fullUrl(), ms);
+                            });
+
+                    // enable the compression
+                    config.http.gzipOnlyCompression(9);
+
+                    // graceful shutdown
+                    config.jetty.modifyServer(server -> server.setStopTimeout(5_000));
+                });
+    }
+
+    /** Add the Routes of Javalin. */
+    private static void addRoute(final @NonNull Route route, final @NonNull Javalin javalin){
+
+        log.debug(
+                "Adding route {} with verb {} in path: {}",
+                route.getClass().getSimpleName(),
+                route.getMethod(),
+                route.getPath());
+
+        switch (route.getMethod()){
+            case GET:
+                javalin.get(route.getPath(), route.getHandler());
+                break;
+            case POST:
+                javalin.post(route.getPath(), route.getHandler());
+                break;
+            case PUT:
+                javalin.put(route.getPath(), route.getHandler());
+                break;
+            default:
+                throw new IllegalArgumentException("Method not supported:" + route.getMethod());
+        }
+    }
 }
