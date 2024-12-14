961232d69b05d50ede5b3aec4b8836e4c1ed425a
BRPortilla <benja.riverap.21@gmail.com>
2024-12-14 10:50:11 -0300

    feat(PersonaLogin): add imports and constructor of PersonaLogin route

--- warehouse/src/main/java/cl/ucn/disc/dsm/pictwin/web/routes/PersonaLogin.java
+++ warehouse/src/main/java/cl/ucn/disc/dsm/pictwin/web/routes/PersonaLogin.java
@@ -1,4 +1,24 @@
 package cl.ucn.disc.dsm.pictwin.web.routes;
 
-public class PersonaLogin {
+import cl.ucn.disc.dsm.pictwin.services.Controller;
+import cl.ucn.disc.dsm.pictwin.web.Route;
+
+import io.javalin.http.Handler;
+import io.javalin.http.HttpStatus;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+
+@Slf4j
+public class PersonaLogin extends Route {
+
+    /** El constructor. */
+    public PersonaLogin(Controller controller){
+        super(Method.POST, "/api/personas");
+
+        //this.handler = buildHandler(controller);
+    }
+
+
+
 }
